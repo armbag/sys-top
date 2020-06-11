f3000ab38cf9873102bb78e4c2abdca9e35b7bff
Armen <armen_bagramian@gmail.com>
2020-06-10 20:57:53 -0400

    front done

--- main.js
+++ main.js
@@ -11,14 +11,15 @@ let mainWindow;
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
-    title: "APP NAME",
-    width: isDev ? 800 : 500,
-    height: 600,
+    title: "SysTop",
+    width: isDev ? 800 : 355,
+    height: 500,
     icon: `${__dirname}/assets/icons/icon.png`,
     resizable: isDev ? true : false,
     backgroundColor: "white",
     webPreferences: {
       nodeIntegration: true,
+      enableRemoteModule: true,
     },
   });
 
