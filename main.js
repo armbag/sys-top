ce7288090aa695f8713f3408b6400759e8f4ffe2
Armen <armen_bagramian@gmail.com>
2020-06-16 14:50:01 -0400

    app done

--- main.js
+++ main.js
@@ -1,141 +1,103 @@
-const path = require('path');
-const { app, BrowserWindow, Menu, ipcMain, Tray } = require('electron');
-const log = require('electron-log');
-const Store = require('./Store');
+const path = require("path");
+const { app, Menu, ipcMain } = require("electron");
+const Store = require("./Store");
+const MainWindow = require("./MainWindow");
+const AppTray = require("./AppTray");
 
 // Set env
-process.env.NODE_ENV = 'developement';
+process.env.NODE_ENV = "production";
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false;
-const isMac = process.platform === 'darwin' ? true : false;
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
 
 let mainWindow;
 let tray;
 
-// init store & defaults
+// Init store & defaults
 const store = new Store({
-	configName: 'user-settings',
-	defaults: {
-		settings: {
-			cpuOverload: 80,
-			alertFrequency: 5,
-		},
-	},
+  configName: "user-settings",
+  defaults: {
+    settings: {
+      cpuOverload: 80,
+      alertFrequency: 5,
+    },
+  },
 });
 
 function createMainWindow() {
-	mainWindow = new BrowserWindow({
-		title: 'SysTop',
-		width: isDev ? 800 : 355,
-		height: 500,
-		icon: `${__dirname}/assets/icons/icon.png`,
-		resizable: isDev ? true : false,
-		// show: false,
-		// opacity: 0.9,
-		// backgroundColor: 'white',
-		webPreferences: {
-			nodeIntegration: true,
-			enableRemoteModule: true,
-		},
-	});
-
-	if (isDev) {
-		mainWindow.webContents.openDevTools();
-	}
-
-	mainWindow.loadFile('./app/index.html');
+  mainWindow = new MainWindow("./app/index.html", isDev);
 }
 
-app.on('ready', () => {
-	createMainWindow();
+app.on("ready", () => {
+  createMainWindow();
 
-	mainWindow.webContents.on('dom-ready', () => {
-		mainWindow.webContents.send('settings:get', store.get('settings'));
-	});
+  mainWindow.webContents.on("dom-ready", () => {
+    mainWindow.webContents.send("settings:get", store.get("settings"));
+  });
 
-	const mainMenu = Menu.buildFromTemplate(menu);
-	Menu.setApplicationMenu(mainMenu);
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
-	mainWindow.on('close', (e) => {
-		if (!app.isQuitting) {
-			e.preventDefault();
-			mainWindow.hide();
-		}
-		return true;
-	});
+  mainWindow.on("close", (e) => {
+    if (!app.isQuitting) {
+      e.preventDefault();
+      mainWindow.hide();
+    }
 
-	const icon = path.join(__dirname, 'assets', 'icons', 'tray_icon.png');
+    return true;
+  });
 
-	tray = new Tray(icon);
+  const icon = path.join(__dirname, "assets", "icons", "tray_icon.png");
 
-	tray.on('click', () => {
-		if (mainWindow.isVisible()) {
-			mainWindow.hide();
-		} else {
-			mainWindow.show();
-		}
-	});
-
-	// mainWindow.on('ready', () => (mainWindow = null));
-	tray.on('right-click', () => {
-		const contextMenu = Menu.buildFromTemplate([
-			{
-				label: 'Quit',
-				click: () => {
-					app.isQuitting = true;
-					app.quit();
-				},
-			},
-		]);
-
-		tray.popUpContextMenu(contextMenu);
-	});
+  // Create tray
+  tray = new AppTray(icon, mainWindow);
 });
 
 const menu = [
-	...(isMac ? [{ role: 'appMenu' }] : []),
-	{
-		role: 'fileMenu',
-	},
-	{
-		label: 'View',
-		submenu: [
-			{
-				label: 'Toggle Navigation',
-				click: () => mainWindow.webContents.send('nav:toggle'),
-			},
-		],
-	},
-	...(isDev
-		? [
-				{
-					label: 'Developer',
-					submenu: [
-						{ role: 'reload' },
-						{ role: 'forcereload' },
-						{ type: 'separator' },
-						{ role: 'toggledevtools' },
-					],
-				},
-		  ]
-		: []),
+  ...(isMac ? [{ role: "appMenu" }] : []),
+  {
+    role: "fileMenu",
+  },
+  {
+    label: "View",
+    submenu: [
+      {
+        label: "Toggle Navigation",
+        click: () => mainWindow.webContents.send("nav:toggle"),
+      },
+    ],
+  },
+  ...(isDev
+    ? [
+      {
+        label: "Developer",
+        submenu: [
+          { role: "reload" },
+          { role: "forcereload" },
+          { type: "separator" },
+          { role: "toggledevtools" },
+        ],
+      },
+    ]
+    : []),
 ];
 
-ipcMain.on('settings:set', (e, value) => {
-	store.set('settings', value);
-	mainWindow.webContents.send('settings:get', store.get('settings'));
+// Set settings
+ipcMain.on("settings:set", (e, value) => {
+  store.set("settings", value);
+  mainWindow.webContents.send("settings:get", store.get("settings"));
 });
 
-app.on('window-all-closed', () => {
-	if (!isMac) {
-		app.quit();
-	}
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
 });
 
-app.on('activate', () => {
-	if (BrowserWindow.getAllWindows().length === 0) {
-		createMainWindow();
-	}
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
 });
 
 app.allowRendererProcessReuse = true;
